52d10bfced3597a1835e23127b98d5a718f6e1d0
gabrielcamba <gabrielcamba@gmail.com>
2025-01-29 16:50:58 -0300

    Primeira inserção de dados no banco com sucesso. Inserido dados de clientes.

--- code/gerar_dados/gerar_dados.py
+++ code/gerar_dados/gerar_dados.py
@@ -1,29 +1,37 @@
 from datetime import date
 from dateutil.relativedelta import relativedelta
 import time
+import psycopg2
 
-
-from gerar_dados_clientes import gerar_dados_clientes
-from gerar_dados_enderecos import gerar_dados_enderecos
-
+from gerar_dados_clientes import cadastrar_cliente
 
 data_nasc_inicio = date(1944,1,1)
 data_nasc_fim = date(2007,12,31)
 data_inicio = date(2020,1,1)
 data_fim = date(2020,1,5)
-
-
-
 data_atual = data_inicio
+
+# Criar conexão única
+conn = psycopg2.connect(
+    dbname="postgres",
+    user="postgres",
+    password="postgres",
+    host="localhost",
+    port="5432"
+)
+cur = conn.cursor()
 
 while data_atual <= data_fim :
     
-    gerar_dados_clientes(data_atual = data_atual,                                               
-                    data_nasc_inicio = data_nasc_inicio,                
-                    data_nasc_fim = data_nasc_fim                    
-                    )
+    cadastrar_cliente(data_atual=data_atual, cur=cur, conn=conn)
+    '''
     time.sleep(5)
     gerar_dados_enderecos(data_atual)
 
+   
+    '''
     data_atual += relativedelta(days=1)
 
+# Fechar conexão após todas as inserções
+cur.close()
+conn.close()
--- code/gerar_dados/gerar_dados_clientes.py
+++ code/gerar_dados/gerar_dados_clientes.py
@@ -4,8 +4,11 @@ from datetime import date
 from unidecode import unidecode
 import pandas as pd
 import os
+import psycopg2
 
-def gerar_dados_clientes(data_atual: date) -> pd.DataFrame:
+def cadastrar_cliente(data_atual: date,
+                      cur, 
+                      conn):
     """
     Gera um DataFrame com dados fictícios de clientes.
     
@@ -18,55 +21,27 @@ def gerar_dados_clientes(data_atual: date) -> pd.DataFrame:
         pd.DataFrame: DataFrame contendo os dados gerados.
     """
     fake = Faker('pt_BR')
-
-    clientes = []  # Lista para armazenar os dados dos clientes
     num_clientes_gerados = random.randint(0,5)
 
-    for i in range(num_clientes_gerados) :
+    for _ in range(num_clientes_gerados) :
+
         if num_clientes_gerados > 0 :
             genero_aleatorio = random.choice(["male", "female"])
             if genero_aleatorio == "male":
-                first_name = fake.first_name_male()
+                primeiro_nome = fake.first_name_male()
                 genero = "M"
             else:
-                first_name = fake.first_name_female()
+                primeiro_nome = fake.first_name_female()
                 genero = "F"
             
-            cpf = fake.cpf()
-            last_name = fake.last_name() 
-            data_nascimento = fake.date_between(start_date= date(1920,1,1), end_date= date(2020,12,31))
-            email = unidecode(f"{first_name}.{last_name}@email.com").lower().replace(" ", "_")
-            telefone = fake.cellphone_number()
-                            
-            # Dicionário com os dados do cliente
-            clientes.append({
-                "ID_CLIENTE": i #Alterar quando consultar do banco de dados
-                "CPF": cpf,
-                "PRIMEIRO_NOME": first_name,
-                "SOBRENOME": last_name,
-                "GENERO": genero,
-                "DATA_NASCIMENTO": data_nascimento,
-                "EMAIL": email,
-                "TELEFONE": telefone,
-                "CRIADO_EM": data_atual,
-                "MODIFICADO_EM": data_atual
-            })
-        
-    # Criando o DataFrame a partir da lista de clientes
-    df_clientes = pd.DataFrame(clientes)
-    file_path = "files/output/clientes.csv"
-    df_clientes.to_csv(file_path, mode='a', header=not os.path.exists(file_path), index=False, sep=';')
-    return df_clientes
-
-'''
-data_nasc_inicio = date(1944,1,1)
-data_nasc_fim = date(2007,12,31)
-data_inicio = date(2020,1,1)
-data_fim = date(2020,1,5)
-data_atual = data_inicio
-
-gerar_dados_clientes(data_atual = data_atual,                                               
-                    data_nasc_inicio = data_nasc_inicio,                
-                    data_nasc_fim = data_nasc_fim                    
-                    )
-'''
+        cpf = fake.cpf()
+        sobrenome = fake.last_name() 
+        data_nascimento = fake.date_between(start_date= date(1920,1,1), end_date= date(2020,12,31))
+        email = unidecode(f"{primeiro_nome}.{sobrenome}@email.com").lower().replace(" ", "_")
+        num_telefone = fake.cellphone_number()
+                        
+        cur.execute(
+        "INSERT INTO oltp.clientes (cpf, primeiro_nome, sobrenome, genero, data_nascimento, email, num_telefone) VALUES (%s, %s, %s, %s, %s, %s, %s);",
+        (cpf, primeiro_nome, sobrenome, genero, data_nascimento, email, num_telefone)
+        )
+        conn.commit()
