d97fad470ec42690a616e452052ceddf98b4ec15
gabrielcamba <gabrielcamba@gmail.com>
2025-01-30 15:06:28 -0300

    Finalizado geração de dados dos preços dos produtos

--- code/gerar_dados/gerar_dados_precos_produtos.py
+++ code/gerar_dados/gerar_dados_precos_produtos.py
@@ -4,7 +4,7 @@ from dateutil.relativedelta import relativedelta
 import random
 
 
-def gera_dados_preco_produtos(data_inicio, data_fim):
+def gera_dados_preco_produtos(cur, conn, data_atual):
     
     """
     Gera um DataFrame com dados de preco de produtos através de um arquivo csv.
@@ -14,44 +14,31 @@ def gera_dados_preco_produtos(data_inicio, data_fim):
     Returns:
         pd.DataFrame: DataFrame contendo os dados gerados.
 
-    """
-    data_inicio = datetime.strptime(data_inicio, "%Y-%m-%d")
-    data_fim = datetime.strptime(data_fim, "%Y-%m-%d")    
+    """  
 
     arq_produtos = pd.read_csv(r"files/input/lista_produtos.csv", sep = ",")
     df_preco_produtos = arq_produtos[["id_produto", "preco"]]
 
-    # Datas de início e fim
-    
-    data_atual = data_inicio
-
     # Lista para armazenar os resultados
     lista_precos = []
 
-    # Percorrendo mês a mês
-    while data_atual <= data_fim:
-        for i, preco in zip(df_preco_produtos["id_produto"], df_preco_produtos["preco"]):
-            # Aplicando uma variação de 10% para cima ou para baixo
-            variacao_preco = preco * random.uniform(0.9, 1.1)
-            
-            # Adicionando os dados à lista
-            lista_precos.append({
-                "ID_PRODUTO": i,
-                "DATA_INICIO": data_atual,
-                "DATA_FIM": data_atual + relativedelta(months=1),
-                "PRECO": round(variacao_preco, 2)  # Arredondando para 2 casas decimais
-            })
-        # Avançando para o próximo mês
-        data_atual += relativedelta(months=1)
+    for i, preco in zip(df_preco_produtos["id_produto"], df_preco_produtos["preco"]):
+        # Aplicando uma variação de 10% para cima ou para baixo
+        variacao_preco = preco * random.uniform(0.9, 1.1)
+        
+        # Adicionando os dados à lista
+        lista_precos.append({
+            "ID_PRODUTO": i,
+            "DATA_INICIO": data_atual,
+            "DATA_FIM": data_atual + relativedelta(months=1),
+            "PRECO": round(variacao_preco, 2)  # Arredondando para 2 casas decimais
+        })
 
     # Criando um DataFrame com os resultados
     df_precos = pd.DataFrame(lista_precos)
-    df_precos.to_csv(r"files/output/precos_produtos.csv",index=False, sep= ";")
-    print(df_precos)
-
-    return df_precos
-
-data_inicio = "2020-1-1"  # Data inicial
-data_fim = "2024-12-31" # Data final
-
-gera_dados_preco_produtos(data_inicio=data_inicio,data_fim=data_fim)
+    
+    cur.executemany(
+        "INSERT INTO oltp.precos_produtos (id_produto, data_inicio, data_fim, preco) VALUES (%s, %s, %s, %s);",
+        df_precos.values.tolist()
+    )
+    conn.commit()
