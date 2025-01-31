66ac306ac976a3bff2d9630ae2a3098d657e1080
gabrielcamba <gabrielcamba@gmail.com>
2025-01-30 21:11:56 -0300

    Script de criação de tabelas ainda não finalizado

--- code/scripts_sql/create_tables.sql
+++ code/scripts_sql/create_tables.sql
@@ -1,3 +1,13 @@
+DROP TABLE oltp.clientes;
+DROP TABLE oltp.enderecos;
+DROP TABLE oltp.clientes_enderecos;
+DROP TABLE oltp.categorias_produtos;
+DROP TABLE oltp.produtos;
+DROP TABLE oltp.precos_produtos;
+DROP TABLE oltp.pedidos;
+DROP TABLE oltp.itens_pedido;
+DROP TABLE oltp.avaliacao_produto;
+
 CREATE TABLE oltp.clientes(
 	id_cliente SERIAL PRIMARY KEY,
 	cpf CHAR(14),
@@ -46,27 +56,44 @@ CREATE TABLE oltp.categorias_produtos(
 );
 
 CREATE TABLE oltp.pedidos(
+	id_pedido SERIAL PRIMARY KEY,
 	id_cliente INTEGER, 
 	id_endereco INTEGER, 
-	metodo_pagamento VARCHAR(15), 
+	metodo_pagamento VARCHAR(20), 
 	num_parcelas INTEGER,
-	data_atual DATE
+	data_pedido DATE
 );
 
 CREATE TABLE oltp.itens_pedido(
-
+	id_item SERIAL,
+	id_pedido INTEGER, 
+	id_produto INTEGER,
+	quantidade INTEGER
 );
 
+CREATE TABLE oltp.avaliacao_produto(
+	id_avaliacao SERIAL,
+	id_produto INTEGER, 
+	nota INTEGER, 
+	comentario TEXT
+);
 
+CREATE TABLE oltp.precos_produtos(
+	id_preco SERIAL PRIMARY KEY,
+	id_produto INTEGER,
+	data_inicio DATE,
+	data_fim DATE,
+	preco DECIMAL
+);
 
-DROP TABLE oltp.clientes;
-DROP TABLE oltp.enderecos;
-DROP TABLE oltp.clientes_enderecos;
-DROP TABLE oltp.categorias_produtos;
-DROP TABLE oltp.produtos;
 
 SELECT * FROM oltp.clientes;
 SELECT * FROM oltp.enderecos;
 SELECT * FROM oltp.clientes_enderecos;
 SELECT * FROM oltp.produtos;
-SELECT * FROM oltp.categorias_produtos;
+SELECT * FROM oltp.precos_produtos;
+SELECT * FROM oltp.pedidos;
+SELECT * FROM oltp.itens_pedido;
+SELECT * FROM oltp.avaliacao_produto;
+
+DELETE FROM oltp.precos_produtos
